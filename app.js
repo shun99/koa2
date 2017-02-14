aa766c856c60cfe4786793a06e7ee774cf1e7a0e
chang shunshun <yghysdr@163.com>
2017-02-14 18:08:02 +0800

    update code

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const Koa = require('koa');
+const router = require('koa-router')();
 const app = new Koa();
 
+
+
 app.use(async(ctx, next)=>{
-    if(ctx.request.path === '/'){
-        ctx.response.body = 'index page';
-    }else{
-        await next();
-    }
+    console.log(`progress ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
 });
-app.use(async(ctx, next)=>{
-    if(ctx.request.path === '/test'){
-        ctx.response.body = 'Test page';
-    }else{
-        await next();
-    }
+
+router.get('/hello/:name', async (ctx, next) =>{
+    let name = ctx.params.name;
+    ctx.response.body = 'hello, ${name}!';
 });
-app.use(async(ctx, next)=>{
-    if(ctx.request.path === '/error'){
-        ctx.response.body = 'Error page';
-    }else{
-        await next();
-    }
+router.get('/', async(ctx, next) =>{
+    ctx.response.body = 'index';
 });
+app.use(router.routes());
+
+
+// app.use(async(ctx, next)=>{
+//     if(ctx.request.path === '/test'){
+//         ctx.response.body = 'Test page';
+//     }else{
+//         await next();
+//     }
+// });
+// app.use(async(ctx, next)=>{
+//     if(ctx.request.path === '/error'){
+//         ctx.response.body = 'Error page';
+//     }else{
+//         await next();
+//     }
+// });
 app.listen(3000);
 console.log('app start at port 3000...');
