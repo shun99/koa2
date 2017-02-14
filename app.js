dc3119d4e1de593fc48e2862bf9a8122a7795923
chang shunshun <yghysdr@163.com>
2017-02-14 18:36:01 +0800

    post body prase

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 const Koa = require('koa');
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 
 
+/**
+ * 提供解析request的body的功能
+ * let name = ctx.request.body.name || '';
+ */
+
+app.use(bodyParser());
 
 app.use(async(ctx, next)=>{
     console.log(`progress ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 });
 
-router.get('/hello/:name', async (ctx, next) =>{
-    let name = ctx.params.name;
-    ctx.response.body = 'hello, ${name}!';
+router.get('/', async (ctx, next)=>{
+      ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
 });
-router.get('/', async(ctx, next) =>{
-    ctx.response.body = 'index';
+
+router.post('/signin', async(ctx, next)=>{
+    let name = ctx.request.body.name || '';
+    let password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if(name === 'koa' && password === '123'){
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    }else{
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
 });
+
+// router.get('/hello/:name', async (ctx, next) =>{
+//     let name = ctx.params.name;
+//     ctx.response.body = 'hello, ${name}!';
+// });
+// router.get('/', async(ctx, next) =>{
+//     ctx.response.body = 'index';
+// });
 app.use(router.routes());
 
 
