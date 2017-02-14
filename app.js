16e60290ae42461defa050ff0cae976fd2354843
chang shunshun <yghysdr@163.com>
2017-02-14 20:59:59 +0800

    update code

--- app.js
+++ app.js
@@ -2,64 +2,33 @@ const Koa = require('koa');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
-
-
-/**
- * 提供解析request的body的功能
- * let name = ctx.request.body.name || '';
- */
-
+const fs = require('fs');
 app.use(bodyParser());
 
-app.use(async(ctx, next)=>{
-    console.log(`progress ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
+var files = fs.readdirSync(__dirname+'/controllers');
+var js_files = files.filter((f)=>{
+    return f.endsWith('.js');
 });
 
-router.get('/', async (ctx, next)=>{
-      ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
-
-router.post('/signin', async(ctx, next)=>{
-    let name = ctx.request.body.name || '';
-    let password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if(name === 'koa' && password === '123'){
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    }else{
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
+for(var f of js_files){
+    console.log(`process controller: ${f}...`);
+    let mapping = require(__dirname+'/controllers' + f);
+    for(var url in mapping){
+        if(url.startsWith('GET ')){
+            var path = url.substring(4);
+            router.get(path, mapping[url]);
+            console.log(`register URL mapping: GET ${path}`);
+        }else if(url.startsWith('POST ')){
+            var path = url.substring(5);
+            router.post(path, mapping[url]);
+            console.log(`register URL mapping: POST ${path}`);
+        }else{
+              // 无效的URL:
+            console.log(`invalid URL: ${url}`);
+        }
     }
-});
+}
 
-// router.get('/hello/:name', async (ctx, next) =>{
-//     let name = ctx.params.name;
-//     ctx.response.body = 'hello, ${name}!';
-// });
-// router.get('/', async(ctx, next) =>{
-//     ctx.response.body = 'index';
-// });
 app.use(router.routes());
-
-
-// app.use(async(ctx, next)=>{
-//     if(ctx.request.path === '/test'){
-//         ctx.response.body = 'Test page';
-//     }else{
-//         await next();
-//     }
-// });
-// app.use(async(ctx, next)=>{
-//     if(ctx.request.path === '/error'){
-//         ctx.response.body = 'Error page';
-//     }else{
-//         await next();
-//     }
-// });
 app.listen(3000);
 console.log('app start at port 3000...');
