1f617c976c8b8ba67bb553d2454da5f8217954b1
chang shunshun <yghysdr@163.com>
2017-02-15 10:55:35 +0800

    update code

--- app.js
+++ app.js
@@ -4,36 +4,9 @@ const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const fs = require('fs');
+const controller = require('./controller');
 
-function addMapping(router, mapping){
-    for(let url in mapping){
-        if(url.startsWith('GET ')){
-            var path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        }else if(url.startsWith('POST ')){
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        }else{
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-function addControllers(router){
-    var files = fs.readdirSync(__dirname+'/controllers');
-    var js_files = files.filter((f)=>{
-        return f.endsWith('.js');
-    });
-    for(let file of js_files){
-        console.log(`process controller: ${file}...`);
-        let mapping = require(__dirname+'/controllers/'+file);
-        addMapping(router, mapping);
-    }
-}
-
-addControllers(router);
+app.use(controller());
 //app.use(bodyParser());必须至少放在app.use(router.routes());之前。
 app.use(bodyParser());
 app.use(router.routes());
