b5843cf9e65d9cebe50a405f89005a218e8a466d
chang shunshun <yghysdr@163.com>
2017-02-15 10:44:21 +0800

    update code

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 "use strict";
-
 const Koa = require('koa');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const fs = require('fs');
 
-// 先导入fs模块，然后用readdirSync列出文件
-// 这里可以用sync是因为启动时只运行一次，不存在性能问题:
-var files = fs.readdirSync(__dirname+'/controllers');
-var js_files = files.filter((f)=>{
-    return f.endsWith('.js');
-});
-
-for(var f of js_files){
-    console.log(`process controller: ${f}...`);
-    /**
-     * 此处时用的是  /controllers/
-     */
-    let mapping = require(__dirname+'/controllers/' + f);
-    for(var url in mapping){
+function addMapping(router, mapping){
+    for(let url in mapping){
         if(url.startsWith('GET ')){
             var path = url.substring(4);
             router.get(path, mapping[url]);
@@ -29,13 +16,25 @@ for(var f of js_files){
             router.post(path, mapping[url]);
             console.log(`register URL mapping: POST ${path}`);
         }else{
-              // 无效的URL:
             console.log(`invalid URL: ${url}`);
         }
     }
 }
 
+function addControllers(router){
+    var files = fs.readdirSync(__dirname+'/controllers');
+    var js_files = files.filter((f)=>{
+        return f.endsWith('.js');
+    });
+    for(let file of js_files){
+        console.log(`process controller: ${file}...`);
+        let mapping = require(__dirname+'/controllers/'+file);
+        addMapping(router, mapping);
+    }
+}
 
+addControllers(router);
+//app.use(bodyParser());必须至少放在app.use(router.routes());之前。
 app.use(bodyParser());
 app.use(router.routes());
 app.listen(3000);
