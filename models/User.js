59a9ca72b86bd702ab1494c8ea049a1f9d27f1cc
chang shunshun <yghysdr@163.com>
2017-02-21 12:38:48 +0800

    first sql

---
+++ models/User.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yghysdr on 2017/2/21.
+ */
+const db = require('../db');
+
+module.exports = db.defineModel('users', {
+    email: {
+        type: db.STRING(100),
+        unique: true
+    },
+    passwd: db.STRING(100),
+    name: db.STRING(100),
+    gender: db.BOOLEAN
+});
+
+
